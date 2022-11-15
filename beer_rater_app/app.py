e70848e6a8d7d84848bf261a615bbcb417d6c2bd
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2022-11-15 13:56:31 -0800

    unknown prev changes

--- beer_rater_app/app.py
+++ beer_rater_app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from os import environ
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super33secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 db = SQLAlchemy(app)
 
